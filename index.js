e2bfcd97e999c425c8cc035e294d0db4dd2d7c91
Alphacode@77 <emmanueloteng.k@gmail.com>
2023-12-18 09:30:40 +0000

    setting up the mongodb with the connection string

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const patientRoutes = require('./routes/patients');
+const express = require("express");
+const mongoose = require("mongoose");
+const patientRoutes = require("./routes/patients");
 
 const app = express();
 
 // MongoDB connection setup
-mongoose.connect('mongodb://localhost:27017/medical_records', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect(
+  "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 app.use(express.json());
 
 // Routes
-app.use('/api/patients', patientRoutes);
+app.use("/api/patients", patientRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
